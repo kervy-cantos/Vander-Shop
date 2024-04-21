56d2e19205a77e1d96df39dcbb1a74aa4e2fceeb
kervy-cantos <krteammain@gmail.com>
2024-04-21 19:52:08 +0800

    add database connection

---
+++ app/api/products/seed/route.js
@@ -0,0 +1,18 @@
+import data from '@/lib/data';
+import dbConnect from '@/lib/dbConnect';
+import ProductModel from '@/lib/models/ProductModel';
+import UserModel from '@/lib/models/UserModel';
+import { NextResponse } from 'next/server';
+
+export const GET = async req => {
+	const { users, products } = data;
+
+	await dbConnect();
+	await UserModel.deleteMany();
+	await UserModel.insertMany(users);
+
+	await ProductModel.deleteMany();
+	await ProductModel.insertMany(products);
+
+	return NextResponse.json({ message: 'Seeded successfully', users, products });
+};
