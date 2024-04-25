42e63af455a23f9500e4edfcd754aa7bdc1546b1
kervy-cantos <krteammain@gmail.com>
2024-04-25 20:58:58 +0800

    fix vercel build error

--- middleware.js
+++ middleware.js
@@ -1,12 +1,34 @@
-export { auth as middleware } from './lib/auth.js';
+import NextAuth from 'next-auth';
+
+const authConfig = {
+	providers: [],
+	callbacks: {
+		authorized({ request, auth }) {
+			const protectedPaths = [
+				/\/shipping/,
+				/\/payment/,
+				/\/place-order/,
+				/\/profile/,
+				/\/order\/(.*)/,
+				/\/admin/
+			];
+			const { pathname } = request.nextUrl;
+			if (protectedPaths.some(p => p.test(pathname))) return !!auth;
+			return true;
+		}
+	}
+};
+
+export const { auth: middleware } = NextAuth(authConfig);
 
 export const config = {
 	matcher: [
 		/*
 		 * Match all request paths except for the ones starting with:
 		 * - api (API routes)
-		 * - _next/image (Image Optimization routes)
-		 * - favicon.ico (Favicon)
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico (favicon file)
 		 */
 		'/((?!api|_next/static|_next/image|favicon.ico).*)'
 	]
